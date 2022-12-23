1e3104212d740ea10669083a66a0b482cc68614f
Ishan Tiwari <poet.infamous@gmail.com>
2022-12-23 10:42:44 +0000

    Prepared navbar with links

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import { About, Help, Home, Logging} from './pages'
 
 const Navbar = () => {
   return (
-    <nav className='bg-blue-500 p-3 text-white'>
-      <h1 className='text-xl'>LogIt</h1>
+    <nav className='bg-blue-500 p-3 text-white flex'>
+      <h1 className='text-xl flex-1'>LogIt</h1>
+      <ul>
+        <a href="/logging" className='mr-2'>Logging</a>
+        <a href='/help' className='mr-2'>Help</a>
+
+        <a href='/' className='mr-1'>Todos</a>
+      </ul>
+
     </nav>
   )
 }
